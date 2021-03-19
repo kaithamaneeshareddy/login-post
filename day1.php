f4b917d84b36fec642594de8d56be3650a96fa4f
kaithamaneeshareddy <80377160+kaithamaneeshareddy@users.noreply.github.com>
2021-03-20 00:10:20 +0530

    Add files via upload

---
+++ day1.php
@@ -0,0 +1,8 @@
+<?php
+$firstname=$_POST["firstname"];
+$surname=$_POST["surname"];
+$mobilenumber=$_POST["mobilenumber"];
+$emailid=$_POST["email"];
+$newpassword=$_POST["newpassword"];
+echo $firstname;
+?>
